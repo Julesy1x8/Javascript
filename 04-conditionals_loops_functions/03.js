763137a5a9c9c44036e4b1e9821fd6dc1aad729d
Jules Berg <julesrberg@gmail.com>
2021-05-26 17:53:49 -0400

    my sixth commit

--- 04-conditionals_loops_functions/03.js
+++ 04-conditionals_loops_functions/03.js
@@ -9,3 +9,25 @@ function getRandomNumber(){
  *    random numbers until it has generated one that is divisible by 11. Do you
  *    remember what operator to use?
  */
+/*
+let number = 5;
+while (number < 1000) {
+    number+= 5;
+    console.log(number);
+}
+
+let number = 101;
+while(number > -100) {
+    number--;
+    console.log(number);
+}
+*/
+let number = 0;
+let repeat = 0;
+while (repeat < 2500) {
+    number += 3;
+    console.log(number);
+    number -= 1;
+    console.log(number);
+    repeat++;
+}
